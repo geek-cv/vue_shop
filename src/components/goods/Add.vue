3ab9bb4b146e1a700713f9a1ce618bae986c202d
geek-cv <1011834386@qq.com>
2020-06-28 11:57:01 +0800

    保存

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -28,7 +28,12 @@
         label-width="100px"
         label-position="top"
       >
-        <el-tabs v-model="activeIndex" :tab-position="'left'">
+        <el-tabs
+          v-model="activeIndex"
+          :tab-position="'left'"
+          :before-leave="beforeTabLeave"
+          @tab-click="tabClicked"
+        >
           <el-tab-pane label="基本信息" name="0">
             <el-form-item label="商品名称" prop="goods_name">
               <el-input v-model="addForm.goods_name"></el-input>
@@ -42,8 +47,28 @@
             <el-form-item label="商品数量" prop="goods_number">
               <el-input v-model="addForm.goods_number" type="number"></el-input>
             </el-form-item>
+            <el-form-item label="商品分类" prop>
+              <el-cascader
+                v-model="addForm.goods_cat"
+                :options="cateList"
+                :props="cateProps"
+                prop="goods_cat"
+                @change="handleChange"
+              ></el-cascader>
+            </el-form-item>
           </el-tab-pane>
-          <el-tab-pane label="商品参数" name="1"></el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">
+            <!-- 渲染表单item项 -->
+            <el-form-item
+            :label="item.attr_name"
+            v-for="item in manyTableData"
+            :key="item.attr_id">
+              <!-- 复选框组 -->
+              <el-checkbox-group v-model="item.attr_vals">
+                <el-checkbox :label="cb" v-for="(cb,i) in item.attr_vals" :key="i" border></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-tab-pane>
           <el-tab-pane label="商品属性" name="2"></el-tab-pane>
           <el-tab-pane label="商品图片" name="3"></el-tab-pane>
           <el-tab-pane label="商品内容" name="4"></el-tab-pane>
@@ -63,17 +88,28 @@ export default {
         goods_name: '',
         goods_price: 0,
         goods_weight: 0,
-        goods_number: 0
+        goods_number: 0,
+        // 商品所属的分类数组
+        goods_cat: []
       },
       // 表单验证规则
       addFormRules: {
         goods_name: [{ required: true, message: '请输入商品名称', trigger: 'blur' }],
         goods_price: [{ required: true, message: '请输入商品价格', trigger: 'blur' }],
         goods_weight: [{ required: true, message: '请输入商品重量', trigger: 'blur' }],
-        goods_number: [{ required: true, message: '请输入商品重量', trigger: 'blur' }]
+        goods_number: [{ required: true, message: '请输入商品重量', trigger: 'blur' }],
+        goods_cat: [{ required: true, message: '请选择商品分类', trigger: 'blur' }]
       },
       // 商品分类列表
-      cateList: []
+      cateList: [],
+      // 商品列表分类
+      cateProps: {
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children'
+      },
+      // 动态参数列表数组
+      manyTableData: []
     }
   },
 
@@ -81,7 +117,14 @@ export default {
     this.getCateList()
   },
 
-  computed: {},
+  computed: {
+    cateId () {
+      if (this.addForm.goods_cat.length === 3) {
+        return this.addForm.goods_cat[2]
+      }
+      return null
+    }
+  },
 
   methods: {
     // 获取所有商品分类数据
@@ -92,6 +135,34 @@ export default {
       }
       this.cateList = res.data
       console.log(res.data)
+    },
+    // 级联选择器选中项变化会触发这个函数
+    handleChange () {
+      if (this.addForm.goods_cat.length !== 3) {
+        this.addForm.goods_cat = []
+      }
+      console.log(this.addForm.goods_cat)
+    },
+    beforeTabLeave (activeName, oldActiveName) {
+      if (oldActiveName === '0' && this.addForm.goods_cat.length !== 3) {
+        this.$message.error('请先选择商品分类！')
+        return false
+      }
+    },
+    async tabClicked () {
+      // console.log(this.activeIndex)
+      // 证明访问的是动态参数面板
+      if (this.activeIndex === '1') {
+        const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`, { params: { sel: 'many' } })
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取动态参数列表失败！')
+        }
+        console.log(res.data)
+        res.data.forEach(item => {
+          item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+        })
+        this.manyTableData = res.data
+      }
     }
   }
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -34,7 +34,9 @@ import {
   Tabs,
   TabPane,
   Steps,
-  Step
+  Step,
+  CheckboxGroup,
+  Checkbox
 } from 'element-ui'
 
 Vue.use(Button)
@@ -70,6 +72,8 @@ Vue.use(Tabs)
 Vue.use(TabPane)
 Vue.use(Steps)
 Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
 
 // message需要全局挂载
 Vue.prototype.$message = Message
